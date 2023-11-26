da6cc6dbb03750d60c0cdaf2c2c8740ae3016ac4
Chef Tech <cheftech@patraldo.com>
2023-11-26 14:12:58 -0600

    first commit

---
+++ src/lib/components/Roller.svelte
@@ -0,0 +1,27 @@
+<script>
+    /** @type {number} */
+    let dice = $state(0);
+    /** @type {Array.<number>} */
+    let rolls = $state([]);
+
+    function roll() {
+        dice = Math.floor(Math.random() * Math.floor(12)) + 1;
+        rolls[rolls.length] = dice;
+    }
+    function reset() {
+        dice = 0;
+        rolls = [];
+    }
+</script>
+
+<section style="padding-top: 50px;">
+        <button on:click={reset}> Let's restart </button>
+        <button on:click={roll}> Let's roll </button>
+
+
+    {#each rolls as t, index}
+        <p style="text-align: center">
+            Roll # {index + 1} =  {t}
+        </p>
+    {/each}
+</section>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Roller from "$lib/components/Roller.svelte";
+</script>
+
+<main class="container">
+    <Roller />
+</main>
